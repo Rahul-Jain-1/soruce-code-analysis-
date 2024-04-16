11ff24d3c5f52fe9d15418a18ba32c4f64e158ca
Rahul-Jain-1 <jainshabrahul@gmail.com>
2024-04-16 19:20:17 +0530

    done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative Ai project",
+    version="0.0.0",
+    author="Rahul jain",
+    author_email="jainshabrahul@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
